6ab135f6ad548a0a32eabbb54386c189d6306036
Matt Hosack <hosack.matt@gmail.com>
2025-12-03 13:45:48 +0100

    add benchmarking

---
+++ internal/bench/days_bench_test.go
@@ -0,0 +1,43 @@
+package bench
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hosackm/aoc2025/internal/days/day01"
+	"github.com/hosackm/aoc2025/internal/days/day02"
+	"github.com/hosackm/aoc2025/internal/days/day03"
+	"github.com/hosackm/aoc2025/internal/runner"
+)
+
+func BenchmarkPackageRunners(b *testing.B) {
+	runners := []struct {
+		name     string
+		r        runner.Day
+		filename string
+	}{
+		{"Day01", &day01.Day01{}, "../../inputs/01.txt"},
+		{"Day02", &day02.Day02{}, "../../inputs/02.txt"},
+		{"Day03", &day03.Day03{}, "../../inputs/03.txt"},
+	}
+
+	input := "some input data"
+
+	for _, tt := range runners {
+		data, _ := os.ReadFile(tt.filename)
+		input = strings.Trim(string(data), "\n")
+
+		b.Run(tt.name+"PartOne", func(b *testing.B) {
+			for b.Loop() {
+				_, _ = tt.r.Part1(input)
+			}
+		})
+
+		b.Run(tt.name+"PartTwo", func(b *testing.B) {
+			for b.Loop() {
+				_, _ = tt.r.Part2(input)
+			}
+		})
+	}
+}
