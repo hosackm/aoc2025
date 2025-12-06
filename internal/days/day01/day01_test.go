531118407f74ebeb965b2a06ffe8d25f00badd78
Matt Hosack <hosack.matt@gmail.com>
2025-12-06 14:53:14 +0100

    fix tests for day 1

--- internal/days/day01/day01_test.go
+++ internal/days/day01/day01_test.go
@@ -2,8 +2,6 @@ package day01
 
 import (
 	"testing"
-
-	"github.com/hosackm/aoc2025/internal/days/inputs"
 )
 
 func TestDay1Part1(t *testing.T) {
@@ -68,37 +66,5 @@ func TestPart2Tip(t *testing.T) {
 	got := dial.GetZeros()
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func BenchmarkPart1(b *testing.B) {
-	data, err := inputs.FS.ReadFile("01.txt")
-	if err != nil {
-		b.Fatalf("couldn't read 01.txt for benchmark")
-	}
-
-	input := string(data)
-	day1 := &Day01{}
-	for b.Loop() {
-		_, err := day1.Part1(input)
-		if err != nil {
-			b.Fatalf("Part1 returned an error")
-		}
-	}
-}
-
-func BenchmarkPart2(b *testing.B) {
-	data, err := inputs.FS.ReadFile("01.txt")
-	if err != nil {
-		b.Fatalf("couldn't read 01.txt for benchmark")
-	}
-
-	input := string(data)
-	day1 := &Day01{}
-	for b.Loop() {
-		_, err := day1.Part2(input)
-		if err != nil {
-			b.Fatalf("Part1 returned an error")
-		}
 	}
 }
